d58bf069e6b80a61ec921e091bca3b0908bfa02b
Sebastián Alejandro Vidal Rojas <vidalrojas.sa@outlook.cl>
2026-07-03 15:05:45 -0400

    chore: change 'id' to 'name' in Widget

--- tkinterform/label.py
+++
@@ -1,7 +0,0 @@
-from tkinter import ttk
-from .widget import Widget
-
-
-class Label(Widget, ttk.Label):
-    def __init__(self, master, id=None, **kwargs):
-        super(Label, self).__init__(id=id, master=master, **kwargs)
--- tkinterform/widget.py
+++ tkinterform/widget.py
@@ -3,7 +3,6 @@ class Widget(object):
     Base class for Tkinterform widgets.
     """
 
-    def __init__(self, id=None, *args, **kwargs):
-        self.id = id
-
+    def __init__(self, *args, **kwargs):
+        self.name = kwargs.pop("name", None)
         super(Widget, self).__init__(*args, **kwargs)
