8c55b701189f0d9e390aa79aa232a3bb086f49d1
Sebastián Alejandro Vidal Rojas <vidalrojas.sa@outlook.cl>
2026-07-06 23:29:51 -0400

    chore: sequence.py: clean up

--- tkinterform/sequence.py
+++ tkinterform/sequence.py
@@ -3,62 +3,69 @@ from .input import Input
 
 
 class Sequence(Input, ttk.Frame):
-    def __init__(self, master, of_form, *args, **kwargs):
+    def __init__(self, master, form, form_padding=None, *args, **kwargs):
         """
         Construct a `tkinterform.Sequence` widget with the parent MASTER.
-
-        It should be registered via `tkinterform.Form.add()`.
         """
-        self.form = of_form
+        self.form = form
+        self.form_padding = form_padding
         self.fieldsets = []
 
         super(Sequence, self).__init__(master, *args, **kwargs)
 
-    def _go_update_fieldsets(self):
-        for form in self.fieldsets:
-            form.event_generate("<<TkfSequenceUpdate>>", when="tail")
+    def add(self, **kwargs):
+        form = self.form(self, **kwargs)
+        form.pack(fill="x", pady=self.form_padding)
 
-    def add(self, refresh_children=True):
-        form = self.form(self)
-        self.append(form)
+        self.fieldsets.append(form)
 
-        if refresh_children:
-            self._go_update_fieldsets()
+        self._go_update_fieldsets()
 
         return form
-
-    def append(self, form):
-        form.pack(fill="x")
-        self.fieldsets.append(form)
 
     def clear(self):
         for form in self.fieldsets:
             form.destroy()
 
         del self.fieldsets[:]
+        self.collapse()
+
+    def collapse(self):
         self.config(width=1, height=1)
 
-    def delete(self, index, refresh_children=True):
-        if 0 <= index < len(self.fieldsets):
-            self.fieldsets.pop(index).destroy()
+    def delete(self, index):
+        self.fieldsets.pop(index).destroy()
+        self._go_update_fieldsets()
 
-            # Collapse into nothing when no fieldsets
-            if not self.fieldsets:
-                self.config(width=1, height=1)
+        if not self.fieldsets:
+            self.collapse()
 
-            if refresh_children:
-                self._go_update_fieldsets()
+    def get(self, index=None):
+        if index is None:
+            return [form.get() for form in self.fieldsets]
 
-    def get(self):
+        return self.fieldsets[index].get()
+
+    def _get_all(self):
         return [form.get() for form in self.fieldsets]
+
+    def _go_update_fieldsets(self):
+        for form in self.fieldsets:
+            form.event_generate("<<TkfSequenceUpdate>>", when="tail")
 
     def is_valid(self):
         return all(form.is_valid() for form in self.fieldsets)
 
-    def set(self, list_):
+    def set(self, value, index=None):
+        if index is None:
+            self._set_all(value)
+        else:
+            self.fieldsets[index].set(value)
+
+    def _set_all(self, values):
         self.clear()
 
-        for element in list_:
-            self.add(refresh_children=False).set(element)
+        for value in values:
+            self.add().set(value)
 
         self._go_update_fieldsets()
