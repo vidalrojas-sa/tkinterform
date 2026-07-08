457dfeddd808b97f6e014fe8ef0b2220d8ab3521
Sebastián Alejandro Vidal Rojas <vidalrojas.sa@outlook.cl>
2026-07-07 20:10:08 -0400

    feat: checkbox.py: add "<<TkfInputUpdate>>" event to Checkbox

--- tkinterform/checkbox.py
+++ tkinterform/checkbox.py
@@ -11,6 +11,12 @@ class Checkbox(Input, ttk.Checkbutton):
         self._do_not_trace = False
         self.checkbox_var = tk.BooleanVar()
 
+        try:
+            self.text_var.trace_add("write", self.on_toggle)
+        except AttributeError:
+            # Fallback for Python<=3.6
+            self.text_var.trace("w", self.on_toggle)
+
         super().__init__(master, variable=self.checkbox_var, *args, **kwargs)
 
     def get(self):
@@ -19,6 +25,11 @@ class Checkbox(Input, ttk.Checkbutton):
     def is_valid(self):
         return True
 
+    def on_toggle(self, *args):
+        if not self._do_not_trace:
+            self.master.event_generate("<<TkfInputUpdate>>", when="tail")
+
     def set(self, value):
-        if isinstance(value, bool):
-            tk.BooleanVar.set(value)
+        with self._trace_stop():
+            if isinstance(value, bool):
+                tk.BooleanVar.set(value)
