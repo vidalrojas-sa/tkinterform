592f4b2cf35c330971890db2daf551398956a470
Sebastián Alejandro Vidal Rojas <vidalrojas.sa@outlook.cl>
2026-07-08 10:56:20 -0400

    fix: combobox.py: improve behavior with undefined text_var

--- tkinterform/combobox.py
+++ tkinterform/combobox.py
@@ -9,9 +9,11 @@ class Combobox(Input, ttk.Combobox):
         """
         Construct a `tkinterform.Combobox` widget with the parent MASTER.
         """
+        self.default = None
         self._do_not_trace = False
         self.required = required
         self.text_var = tk.StringVar()
+        self.values = {}
 
         try:
             self.text_var.trace_add("write", self.on_select)
@@ -27,6 +29,64 @@ class Combobox(Input, ttk.Combobox):
             **kwargs
         )
 
+    def add_option(self, name, value, default=False):
+        if default or self.default is None:
+            self.default = name
+
+        self.values[name] = value
+        self["values"] = self.keys()
+
+        if default or not self.text_var.get():
+            self.set_value(self.get_value())
+
+    def delete_option(self, name):
+        if self.default == name:
+            self.default = self.keys()[0] if self.keys() else None
+
+        del self.values[name]
+        self["values"] = self.keys()
+
+        if self.text_var.get() == name:
+            self.set_value(None)
+
+    def get_value(self):
+        return self.values.get(self.text_var.get(), "")
+
+    def get_name_at_value(self, value):
+        for name, _value in self.values.items():
+            if _value == value:
+                return name
+        return None
+
+    def has(self, name):
+        return name in self.values
+
+    def is_valid(self):
+        if self.required and not self.get_value():
+            return False
+
+        return True
+
+    def keys(self):
+        """
+        Returns a list of all keys contained in the Form instance.
+        The keys are strings.
+        """
+        return list(self.values)
+
+    def on_select(self, *args):
+        if not self._do_not_trace:
+            self.master.event_generate("<<TkfInputUpdate>>", when="tail")
+
+    def set_value(self, name):
+        name = self.get_name_at_value(name)
+
+        with self._trace_stop():
+            if name is not None:
+                self.set(name)
+            else:
+                self.set(self.default if self.default else "")
+
     @contextmanager
     def _trace_stop(self):
         self._do_not_trace = True
@@ -34,21 +94,3 @@ class Combobox(Input, ttk.Combobox):
             yield
         finally:
             self._do_not_trace = False
-
-    def get(self):
-        return self.text_var.get().strip()
-
-    def is_valid(self):
-        if self.required and not self.get():
-            return False
-
-        return True
-
-    def on_select(self, *args):
-        if not self._do_not_trace:
-            self.master.event_generate("<<TkfInputUpdate>>", when="tail")
-
-    def set(self, string):
-        with self._trace_stop():
-            if string is not None and string != "":
-                self.set(string)
